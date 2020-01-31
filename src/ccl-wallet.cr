a42d0158ad97945b0f4ec9df7ee8db02971dad57
Cristian Șerb <cserb@pm.me>
2020-01-31 15:32:33 +0100

    Creating, signing & propagating transaction

--- src/ccl-wallet.cr
+++ src/ccl-wallet.cr
@@ -2,6 +2,7 @@ require "secp256k1"
 require "commander"
 require "big"
 require "json"
+require "http/client"
 
 module CCL::Wallet
   VERSION = "0.1.0"
@@ -9,10 +10,83 @@ module CCL::Wallet
 
   alias PrivKey = String
   alias Address = String
+  alias TxnHash = String
 
   def generate : PrivKey
     eth = Secp256k1::Ethereum::Account.new
     eth.get_secret
+  end
+
+  def address(priv_key) : Address
+    pp priv_key
+    Secp256k1::Ethereum.address_from_private(BigInt.new(priv_key, 16))
+  end
+
+  def sign(priv_key : PrivKey, txn_hash : TxnHash) : NamedTuple
+    sig = Secp256k1::Signature.sign(txn_hash, BigInt.new(priv_key, 16))
+    {r: sig.r, s: sig.s}
+  end
+
+  struct Signature
+    include JSON::Serializable
+
+    getter r : String
+    getter s : String
+
+    def initialize(@r, @s)
+    end
+  end
+
+  abstract struct AbstractTransaction
+    include JSON::Serializable
+
+    getter hash : TxnHash
+    getter amount : UInt64
+    getter timestamp : Int64
+    @[JSON::Field(emit_null: true)]
+    property sig : Signature?
+
+    private def calc_hash(*seed) : TxnHash
+      sha = OpenSSL::Digest.new("SHA256")
+      sha.update(seed.join(""))
+      sha.hexdigest
+    end
+  end
+
+  struct Transaction < AbstractTransaction
+    getter from : String
+    getter to : String
+
+    def initialize(@from, @to, @amount)
+      @timestamp = Time.utc.to_unix
+      @hash = calc_hash(from, to, amount, timestamp)
+    end
+  end
+
+  struct Stake < AbstractTransaction
+    getter staker : String
+
+    def initialize(@staker, @amount)
+      @timestamp = Time.utc.to_unix
+      @hash = calc_hash(staker, amount, timestamp)
+    end
+  end
+
+  module Client
+    extend self
+
+    def post_transaction(node : String, txn : Transaction)
+      res = HTTP::Client.post(
+        "http://#{node}/transactions",
+      headers: HTTP::Headers{
+        "Content-Type" => "application/json",
+        "UserAgent"    => "CCL-Wallet",
+      },
+        body: txn.to_json
+      )
+
+      pp res
+    end
   end
 
   module Command
@@ -20,24 +94,43 @@ module CCL::Wallet
       extend self
 
       def call(options)
+        pp options
         if !options.int["amount"]
           puts "You must specify an amount (e.g. --amount 1)"
           return
         end
-        if !options.int["recepient"]
-          puts "You must specify the recipient (e.g. --recepient 0xd8eCB3556006A94298f09274Af15eB29e81329D5)"
+        if !options.string["recepient"]
+          puts "You must specify the recepient (e.g. --recepient 0xd8eCB3556006A94298f09274Af15eB29e81329D5)"
           return
         end
         if !options.string["node"]
-          puts "You must specify a node (e.g. --recepient 127.0.0.1:3000)"
+          puts "You must specify a node (e.g. --node 127.0.0.1:3000)"
           return
         end
 
         recepient_address = options.string["recepient"]
+        amount = options.int["amount"]
+        address = CCL::Wallet.address(CCL::Wallet::Store.read)
+        node = options.string["node"]
 
-        # POST unsigned transaction and receive the hash
-        # sign the hash
-        # PUT finish transaction
+        unsigned_txn = Transaction.new(
+          from: address,
+          to: recepient_address,
+          amount: amount.to_u64
+        )
+
+        raw_signature = CCL::Wallet.sign(
+          CCL::Wallet::Store.read,
+          txn_hash: unsigned_txn.hash
+        )
+        signature = Signature.new(
+          r: raw_signature[:r].to_s,
+          s: raw_signature[:s].to_s
+        )
+        unsigned_txn.sig = signature
+        signed_txn = unsigned_txn
+
+        CCL::Wallet::Client.post_transaction(node, signed_txn)
       end
 
       def address : Address
